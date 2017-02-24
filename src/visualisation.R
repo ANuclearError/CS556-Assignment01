de01d816868556895c099c1ba576204245bb9b5d
Aidan O'Grady <aidan@aidanogrady.com>
2017-02-24 16:45:45 +0000

    More stuff.

--- src/visualisation.R
+++ src/visualisation.R
@@ -5,6 +5,8 @@ library(ggplot2)
 
 
 steam <- read.csv("data/top100in2weeks.csv")
+
+# Generate tags dataframe.
 tags <- data.frame(v=numeric(201), w=numeric(201), x=numeric(201),
     y=numeric(201), z=numeric(201));
 tags[,1] <- colnames(steam[,-(1:16)])
@@ -13,7 +15,14 @@ tags[,3] <- t(steam[102,-(1:16)])
 tags[,4] <- t(steam[103,-(1:16)])
 tags[,5] <- t(steam[104,-(1:16)])
 colnames(tags) = c("Tag", "Tagged", "RelTagged", "AvgScore", "RelAvgScore")
+
+# Handles steam data frame
 steam <- steam[1:100,]
+steam$ScoreGrade <- cut(steam$ScoreRank, breaks=seq(0, 100, by=10),
+    labels=seq(0, 9, by=1))
+steam$PriceBand <- cut(steam$Price, breaks=seq(0, 6000, by=500),
+    labels=seq(0, 12, by=1))
+
 
 # ScoreRank visualisation
 score_rank_hist <- ggplot(steam, aes(x=ScoreRank)) +
@@ -43,6 +52,7 @@ score_price <- ggplot() +
     geom_rect(data=score_price_rects,
         aes(xmin=xstart, xmax=xend, ymin=ystart, ymax=yend, fill=col, alpha=.25)) + 
     geom_point(data=steam, aes(ScoreRank, Price)) +
+    geom_smooth() +
     scale_x_continuous(breaks=seq(0, 100, by=10), limits=c(0,100)) +
     scale_y_continuous(breaks=seq(0, 6000, by=500), limits=c(0,6000)) +
     theme(legend.position="none")
@@ -58,8 +68,9 @@ pub_bar <- ggplot(pub, aes(x=Publisher, y=AvgScore)) +
 
 
 # Tag visualisation
-tag_score <- ggplot(tags, aes(Tagged, AvgScore)) + geom_point()
-tag_score_rel <- ggplot(tags, aes(RelTagged, RelAvgScore)) + geom_point()
+tag_score <- ggplot(tags, aes(Tagged, AvgScore)) + geom_point() + geom_smooth()
+tag_score_rel <- ggplot(tags, aes(RelTagged, RelAvgScore)) + geom_point() +
+    geom_smooth()
 
 
 save_all <- function() 
