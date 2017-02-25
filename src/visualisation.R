a84cd4ee3b9d0121b34715c2ed5561ea6cb67b99
Aidan O'Grady <aidan@aidanogrady.com>
2017-02-25 22:37:49 +0000

    Finally finsihed data visualisation and preparation parts of the report.

--- src/visualisation.R
+++ src/visualisation.R
@@ -18,10 +18,8 @@ colnames(tags) = c("Tag", "Tagged", "RelTagged", "AvgScore", "RelAvgScore")
 
 # Handles steam data frame
 steam <- steam[1:100,]
-steam$ScoreGrade <- cut(steam$ScoreRank, breaks=seq(0, 100, by=10),
-    labels=seq(0, 9, by=1))
-steam$PriceBand <- cut(steam$Price, breaks=seq(0, 6000, by=500),
-    labels=seq(0, 12, by=1))
+steam$ScoreGrade <- cut(steam$ScoreRank, seq(0, 100, by=10))
+steam$PriceBand <- cut(steam$Price, seq(0, 6000, by=500))
 
 
 # ScoreRank visualisation
@@ -49,10 +47,10 @@ score_price_rects <- data.frame(xstart=c(0,50,0,50), xend=c(50,100,50,100),
     ystart=c(0,0,3000,3000), yend=c(3000,3000,6000,6000), col=letters[1:4])
 
 score_price <- ggplot() +
-    geom_rect(data=score_price_rects,
-        aes(xmin=xstart, xmax=xend, ymin=ystart, ymax=yend, fill=col, alpha=.25)) + 
     geom_point(data=steam, aes(ScoreRank, Price)) +
     geom_smooth() +
+    geom_rect(data=score_price_rects,
+        aes(xmin=xstart, xmax=xend, ymin=ystart, ymax=yend, fill=col, alpha=.25)) + 
     scale_x_continuous(breaks=seq(0, 100, by=10), limits=c(0,100)) +
     scale_y_continuous(breaks=seq(0, 6000, by=500), limits=c(0,6000)) +
     theme(legend.position="none")
@@ -73,12 +71,36 @@ tag_score_rel <- ggplot(tags, aes(RelTagged, RelAvgScore)) + geom_point() +
     geom_smooth()
 
 
+# The playtime point graphs
+avg_f <- ggplot(steam, aes(x=AverageForever, y=ScoreRank)) +
+    geom_point() + geom_smooth()
+avg_t <- ggplot(steam, aes(x=Average2Weeks, y=ScoreRank)) +
+    geom_point() + geom_smooth()
+med_f <- ggplot(steam, aes(x=MedianForever, y=ScoreRank)) +
+    geom_point() + geom_smooth()
+med_t <- ggplot(steam, aes(x=Median2Weeks, y=ScoreRank)) +
+    geom_point() + geom_smooth()
+
+# Owners
+owners <- ggplot(steam, aes(x=Owners, y=ScoreRank)) +
+    geom_point() + geom_smooth()
+players_f <- ggplot(steam, aes(x=PlayersForever, y=ScoreRank)) +
+    geom_point() + geom_smooth()
+players_t <- ggplot(steam, aes(x=Players2Weeks, y=ScoreRank)) +
+    geom_point() + geom_smooth()
+
 save_all <- function() 
     ggsave("doc/report/img/score_rank.png", score_rank_hist) +
     ggsave("doc/report/img/price.png", price_hist) +
     ggsave("doc/report/img/score_price.png", score_price) +
     ggsave("doc/report/img/publishers.png", pub_bar) +
     ggsave("doc/report/img/tag_score.png", tag_score) +
-    ggsave("doc/report/img/tag_score_rel.png", tag_score_rel)
-
+    ggsave("doc/report/img/tag_score_rel.png", tag_score_rel) +
+    ggsave("doc/report/img/avg_f.png", avg_f) +
+    ggsave("doc/report/img/avg_t.png", avg_t) +
+    ggsave("doc/report/img/med_f.png", med_f) +
+    ggsave("doc/report/img/med_t.png", med_t) +
+    ggsave("doc/report/img/owners.png", owners) +
+    ggsave("doc/report/img/players_f.png", players_f) +
+    ggsave("doc/report/img/players_t.png", players_t)
 # save_all()
